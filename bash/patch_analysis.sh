e0988b21818c979a1cc905682f883e1ffb2411d7
ryanthomas <ryan.thomas.rt@gmail.com>
2017-03-16 01:42:25 +0800

    improved comments, names, uniform classification

--- bash/patch_analysis.sh
+++ bash/patch_analysis.sh
@@ -25,7 +25,6 @@ with bounds:        $BOUNDS
 #################################
 "
 
-
 g.mapset -c  mapset=$NAME location=urban dbase=$GRASSDB 
 
 v.external  $1 layer=$NAME --overwrite
@@ -38,32 +37,41 @@ r.reclass   input=land_cover@$NAME    output=urban@$NAME   --overwrite rules=- <
 EOF
 
 # clump the contiguous land uses together with the diagonals included.
-r.clump   -d  --overwrite   input=urban@$NAME   output=urban_clumps@$NAME --quiet
-#calculate the area of each clump
-echo "Urban Clumps"
-r.area input=urban_clumps@$NAME  output=large_clumps@$NAME   --overwrite   lesser=8 --quiet  #is 8 right threshold?  
+r.clump   -d  --overwrite   input=urban@$NAME   output=all_clumps@$NAME --quiet
+
+###########################################################################
+# Steps to mak the mask
+
+# 1. Select the big clumps
 # assign clumps with area > 4km^2 to 1, the rest to 0
-# Make a buffer of 20000 m
-r.grow.distance -m  input=large_clumps@$NAME distance=meters_from_urban_area@$NAME  metric=geodesic --quiet --overwrite
-#if the area is bigger than 2 km sq, don't include it in the expansion
-r.reclass   input=meters_from_urban_area@$NAME   output=urban_peri@$NAME --overwrite --quiet rules=- << EOF
+r.area input=all_clumps@$NAME  output=large_clumps@$NAME   --overwrite   lesser=8 --quiet   
+# TODO: Is 8 right threshold? 
+
+# 2. Make a buffer of 20000 m
+r.grow.distance -m  input=large_clumps@$NAME distance=meters_from_large_clumps@$NAME  metric=geodesic --quiet --overwrite
+r.reclass   input=meters_from_large_clumps@$NAME   output=buffer@$NAME --overwrite --quiet rules=- << EOF
 0 thru 2000 = 1 urban
 *   = NULL
 EOF
 
-# clump the contiguous land uses together with the diagonals included.
-r.clump -d --overwrite input=urban_peri@$NAME   output=urban_peri_clumps@$NAME --quiet
-BIG=$(r.report -n urban_peri_clumps@$NAME  units=c sort=asc | awk -F "|" '{ print $2 }' | tail -n 4 | head -n 1)
+# 3. Clump the buffered urban land uses.
+r.clump -d --overwrite input=buffer@$NAME   output=extended_urban_area@$NAME --quiet
+# Select the biggest clump as the central urban area.
+BIG=$(r.report -n extended_urban_area@$NAME  units=c sort=asc | awk -F "|" '{ print $2 }' | tail -n 4 | head -n 1)
+r.mapcalc  "buffer = if(extended_urban_area==$BIG,1,null())" --overwrite --quiet
 
-#intersect the quito_urban file with the buffered tif
-r.mapcalc  "urban_mask = if(urban_peri_clumps==$BIG,1,null())" --overwrite --quiet
-
+# 4. Calculate the urban areas including partial intersections.
 # -c uses circular neighbors; add 0 values to 
-r.neighbors -c input=urban_mask@$NAME selection=urban_clumps@$NAME    output=urban_nbhd@$NAME  method=stddev size=7 --overwrite --quiet
-r.mask      raster=urban_nbhd@$NAME --quiet
-r.mapcalc   "urban_agglomeration = urban_clumps" --overwrite --quiet
+r.neighbors -c input=buffer@$NAME selection=all_clumps@$NAME    output=buffer_mask@$NAME  method=stddev size=7 --overwrite --quiet
+r.mask      raster=buffer_mask@$NAME --quiet
+r.mapcalc   "urban_agglomeration = all_clumps" --overwrite --quiet
 r.mask -r
 
+# Reclassify all areas with STDEV of 0 or 1 to be part of the urban agglomeration.
+r.reclass    input=extended_urban_area@$NAME   output=urban_agglomeration@$NAME --overwrite --quiet rules=- << EOF
+* = 1 urban
+EOF
+    
 
 r.li.padcv          input=urban_agglomeration@$NAME config=patch_index       output=padcv_$NAME         --overwrite --quiet
 r.li.patchdensity   input=urban_agglomeration@$NAME config=patch_index       output=patchdensity_$NAME  --overwrite --quiet
