eebdedfe30065f1d374600f7674d03aee475cac3
ryanthomas <ryan.thomas.rt@gmail.com>
2017-05-08 20:55:25 -0400

    functioning properly -- notice succint workflow

--- bash/07_greenspace.sh
+++ bash/07_greenspace.sh
@@ -12,22 +12,50 @@
 # 
 #############################################################################
 # This file simply calls the grass_patch_statistics file on a folder of shapefiles.
+
 DIR=$(echo $PWD)
 
-export DATA=${DIR}/data/
+export DATA=${DIR}/data
 export IND=${DIR}/indicators
-export SH=${DIR}/source/bash/
-export GRASSDB=${DIR}/grassdb/
-export RAS=${DIR}/data/raster/    # all and only raster data goes here
-export VEC=${DIR}/data/vector/  # all and only vector data goes here.
-export TMP=${DIR}/data/tmp/      # used to download and unzip files.
+export SH=${DIR}/source/bash
+export GRASSDB=${DIR}/grassdb
+export RAS=${DIR}/data/raster   # all and only raster data goes here
+export VEC=${DIR}/data/vector  # all and only vector data goes here.
+export TMP=${DIR}/data/tmp     # used to download and unzip files.
 
 
 echo "Calculating greenspace statistics."
 
-for city in ${VEC}/city_green/*_treecover.shp ; do
-    
-    bash $SH/greenspace.sh $city ; done
+
+
+LOCATION_NAME=urban
+NAME=$(echo `basename $CITY` | awk -F '[._]' '{ print $1 }')
+BOUNDS=$(ogrinfo -al  $CITY  | grep "Extent: " | awk -F "[ (,)]" '{ print ("n="$5+2,"s="$11-2, "e="$9+2, "w="$3-2) }' )
+
+
+echo "
+#################################
+Working on city:    $NAME 
+with bounds         $BOUNDS
+#################################
+"
+
+# set mapping region
+g.mapset -c ${NAME}
+g.region vector=${NAME}
+
+# NOTE: v.external (as used in previous script) does not bring in attributes.
+# TODO: Fix projection issue
+v.in.ogr ${VEC}/greenspaces/${NAME}.shp  snap=10e-7  output=parks --overwrite
+#v.in.ogr ${VEC}/greenspaces/${NAME}_parks.shp  snap=10e-7 output=london_grn --overwrite
+v.overlay ainput=parks binput=${NAME} operator="and" output=nbhd_parks snap=.000001 --overwrite 
+v.db.addcolumn nbhd_parks col="area DOUBLE PRECISION"  --overwrite 
+v.to.db map=nbhd_parks@${NAME} layer=1 qlayer=1 option=area units=meters columns=area  --overwrite 
+v.centroids input=nbhd_parks output=park_cent option=add   --overwrite 
+v.vect.stats points=nbhd_parks areas=${NAME} type=centroid method=sum count_column="parks" points_column=area stats_column="park_area"   --overwrite 
+
+
+
    
 #mkdir -p $DATA/stats/
 #for file in ${VEC}/air**.csv; do
--- bash/greenspace.sh
+++ bash/greenspace.sh
@@ -16,6 +16,11 @@ LOCATION_NAME=urban
 NAME=$(echo `basename $1` | awk -F '[._]' '{ print $1 }')
 BOUNDS=$(ogrinfo -al  $1  | grep "Extent: " | awk -F "[ (,)]" '{ print ("n="$5+2,"s="$11-2, "e="$9+2, "w="$3-2) }' )
 
+NAME=london
+BOUNDS="n=53.3799 s=49.6084 e=2.24755 w=-2.41724"
+
+
+
 echo "
 #################################
 Working on city:    $NAME 
@@ -28,14 +33,18 @@ g.mapset -c ${NAME}
 g.region vector=${NAME}
 
 # NOTE: v.external (as used in previous script) does not bring in attributes.
-v.in.ogr ${VEC}/city_green/${NAME}_treecover.shp  snap=10e-7  --overwrite
-v.in.ogr ${VEC}/city_green/${NAME}_parks.shp  snap=10e-7  --overwrite
-v.in.osm
-v.db.join map=${NAME} column=cat other_table=${NAME}_parks other_column=cat --overwrite
-v.db.join map=${NAME} column=cat other_table=${NAME}_treecover other_column=cat --overwrite
+# TODO: Fix projection issue
+v.in.ogr ${VEC}/greenspaces/${NAME}.shp  snap=10e-7  output=parks --overwrite
+#v.in.ogr ${VEC}/greenspaces/${NAME}_parks.shp  snap=10e-7 output=london_grn --overwrite
+v.overlay ainput=parks binput=${NAME} operator="and" output=nbhd_parks snap=.000001 --overwrite 
+v.db.addcolumn nbhd_parks col="area DOUBLE PRECISION"
+v.to.db map=nbhd_parks@${NAME} layer=1 qlayer=1 option=area units=meters columns=area
+v.centroids input=nbhd_parks output=park_cent option=add
+v.vect.stats points=nbhd_parks areas=${NAME} type=centroid method=sum count_column="parks" points_column=area stats_column="park_area"
 
 
-mkdir -p ${DATA}stats/greenspace
-v.out.ogr -c input=${NAME}@${NAME} layer=${NAME} output=${DATA}stats/greenspace/${NAME}.csv format="CSV"  --overwrite --quiet
-mkdir -p ${DATA}/vector/out/
-v.out.ogr -c input=${NAME}@${NAME} layer=${NAME} output=${DATA}/vector/out/${NAME}.shp   --overwrite --quiet
+
+#mkdir -p ${DATA}stats/greenspace
+#v.out.ogr -c input=${NAME}@${NAME} layer=${NAME} output=${DATA}stats/greenspace/${NAME}.csv format="CSV"  --overwrite --quiet
+#mkdir -p ${DATA}/vector/out/
+#v.out.ogr -c input=${NAME}@${NAME} layer=${NAME} output=${DATA}/vector/out/${NAME}.shp   --overwrite --quiet
